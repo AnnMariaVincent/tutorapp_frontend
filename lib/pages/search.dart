c36a72d7c2e09a16da9d9435c75098d2b677c1c4
AnnMariaVincet <annmariavincent05@gmail.com>
2024-02-09 00:13:30 +0530

    search  tutor

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -14,10 +14,62 @@ class _searchState extends State<search> {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.purple.withOpacity(.50),
+        ),
 
+        body: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            height: 700,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [
+                      Colors.pinkAccent.withOpacity(.3),
+                      Colors.lightBlueAccent.withOpacity(.2),
+                      Colors.lightBlueAccent.withOpacity(.2),
+                      Colors.purpleAccent.withOpacity(.1),
+                      Colors.pinkAccent.withOpacity(.3),
 
+                    ]
+                )
+            ),
+
+            padding: EdgeInsets.all(50),
+            child: Column(
+                children: [
+                  SizedBox(height: 30,),
+                  TextField(
+
+                    decoration: InputDecoration(
+                        labelText: ("Search Tutor"),
+                        hintText: "Search Tutor",
+                        border: OutlineInputBorder()
+                    ),
+                  ),
+
+                  SizedBox(height: 20,width: 250,),
+                  SizedBox(
+                    height: 60,
+                    width: 200,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black12.withOpacity(.2),
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.zero
+                          )
+                      ),
+                      onPressed: () {
+                      },
+                      child: Text("SEARCH"),
+                    ),
+                  ),
+
+                ]
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
